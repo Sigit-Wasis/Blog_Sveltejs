1e412786acaac394ee913c3fbb47f1d17428c39d
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-01-19 11:18:56 +0700

    navigasi menu

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,31 @@
+<svelte:head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</svelte:head>
+
 <script>
-	export let name;
+	import  { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'sveltestrap';
+	let isOpen = false;
+
+	function handleUpdate(event) {
+    	isOpen = event.detail.isOpen;
+	}
+	
+	let name = 'world';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<h3>Selamat Datang Di QODR.OR.ID</h3>
-	<p>Latihan Belajar Menggunakan Framework Javascript Svelte.</p>
-</main>
+	<Navbar color="dark" dark expand="md">
+	  	<NavbarBrand href="/">Blog</NavbarBrand>
+	  	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
+	  	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+	    	<Nav class="ml-auto" navbar>
+	      		<NavItem>
+	         		<NavLink > Beranda </NavLink>
+	      		</NavItem>
+	      		<NavItem>
+	         		<NavLink > Tentang Kami </NavLink>
+	      		</NavItem>
+	    	</Nav>
+	  	</Collapse>
+	</Navbar>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<h1>Hello {name}!</h1>
