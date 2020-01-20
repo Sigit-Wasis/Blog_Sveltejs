d7f1f43b217992b6b8c202ea7b7c90d59a03870d
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-01-20 08:20:12 +0700

    Jumbotron Svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,29 +3,46 @@
 </svelte:head>
 
 <script>
+  // Import Components Navigasi
 	import  { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'sveltestrap';
+  // Import Components Jumbroton
+  import { Button, Jumbotron } from 'sveltestrap';
+
 	let isOpen = false;
 
 	function handleUpdate(event) {
     	isOpen = event.detail.isOpen;
 	}
-	
-	let name = 'world';
 </script>
 
-	<Navbar color="dark" dark expand="md">
-	  	<NavbarBrand href="/">Blog</NavbarBrand>
-	  	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
-	  	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
-	    	<Nav class="ml-auto" navbar>
-	      		<NavItem>
-	         		<NavLink > Beranda </NavLink>
-	      		</NavItem>
-	      		<NavItem>
-	         		<NavLink > Tentang Kami </NavLink>
-	      		</NavItem>
-	    	</Nav>
-	  	</Collapse>
-	</Navbar>
-
-<h1>Hello {name}!</h1>
+<Navbar color="dark" dark expand="md">
+  	<NavbarBrand href="/">Blog</NavbarBrand>
+  	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
+  	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+    	<Nav class="ml-auto" navbar>
+      		<NavItem>
+         		<NavLink > Beranda </NavLink>
+      		</NavItem>
+      		<NavItem>
+         		<NavLink > Tentang Kami </NavLink>
+      		</NavItem>
+      		<NavItem>
+         		<NavLink > Kontak </NavLink>
+      		</NavItem>
+    	</Nav>
+  	</Collapse>
+</Navbar>
+<Jumbotron>
+    <h5 class="display-3">Selamat Datang!</h5>
+    <p class="lead">
+        Kami ucapkan selamat datang di blog ini semoga bermanfaat.
+    </p>
+    <hr class="my-2" />
+    <p>
+      It uses utility classes for typography and spacing
+      to space content out within the larger container.
+    </p>
+    <p class="lead">
+        <Button color="primary">Selengkapnya</Button>
+    </p>
+</Jumbotron>
